8b9a56c35cede3b2cb4e89f3b5d85ae5e5f0cbda
Arjun Bala <arjun.bala@darshan.ac.in>
2023-07-28 10:31:54 +0530

    changd to 0.7

--- first.java
+++ first.java
@@ -1,7 +1,7 @@
 public class first{
     public static void main(String[] args) {
-        for(int i=0;i<1;i++){
-            String side = Math.random()>0.4?"Left":"Right";
+        for(int i=0;i<7;i++){
+            String side = Math.random()>0.7?"Left":"Right";
             int rowNumber = (int)Math.ceil(Math.random()*30);
             int columnNumber = (int)Math.ceil(Math.random()*10);
             System.out.println(side+"-"+rowNumber+"-"+columnNumber);
